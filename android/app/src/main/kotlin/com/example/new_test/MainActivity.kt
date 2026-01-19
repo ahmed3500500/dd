722fa8c082fb963ceebcf73710cc52316457a6ed
ahmed3500500 <ahmed3500599@gmail.com>
2026-01-18 22:58:14 -0600

    first commit

--- android/app/src/main/kotlin/com/example/new_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/new_test/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.skoon.muslim.app
 
-import androidx.annotation.NonNull
-import com.ryanheise.audioservice.AudioServiceActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : AudioServiceActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
-}
+class MainActivity : FlutterActivity()
